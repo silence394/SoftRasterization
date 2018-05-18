0252865e6bbea197a9d0d52b0fca6bd575b3a713
silence394 <silence394@126.com>
2018-05-18 22:37:27 +0800

    add vertexformat.

--- SoftRenderer/StaticMesh.h
+++ SoftRenderer/StaticMesh.h
@@ -10,6 +10,14 @@ struct StaticMeshResouce
 
 class StaticMesh
 {
+public:
+	enum EVertexFormat
+	{
+		VF_POSITION		= 0x01,
+		VF_NORMAL		= 0x02,
+		VF_TEXCOORD0	= 0x04,
+	};
+
 private:
 	std::vector<StaticMeshResouce>	mResources;
 
